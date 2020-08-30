17999deb2bdc8d9bcdd7a15efb5717409c32be37
Lu Li <xmas1984@hotmail.com>
2020-08-29 21:54:16 -0600

    Initial Project

---
+++ GroupBuyingAddon/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace GroupBuyingAddon
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
